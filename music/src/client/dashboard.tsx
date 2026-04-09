8ac435e1a2d429656fe4c33d2bfe9babf5fd85d1
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2026-04-10 02:18:01 +0700

    Update dashboard.tsx

--- music/src/client/dashboard.tsx
+++ music/src/client/dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect, useCallback, useNavigate } from "react";
 import "../App.css";
 import {
   FaHeadphonesAlt,
@@ -26,6 +26,8 @@ const MusicPlayer = () => {
 
   const audioRef = useRef<HTMLAudioElement>(null);
   const progressRef = useRef(null);
+
+  const navigate = useNavigate();
 
   const API_BASE = import.meta.env.VITE_API_URL || '';
 
