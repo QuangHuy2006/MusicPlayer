db7e140071b3f882a190f62230a12f2a623bbd8d
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2026-04-11 23:05:44 +0700

    Update layout.tsx

--- music/src/layout/layout.tsx
+++ music/src/layout/layout.tsx
@@ -73,20 +73,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
       {/* ===== MAIN CONTENT ===== */}
       <main className="flex-1 container mx-auto px-4 py-6 relative">
-        <div className="absolute top-5 right-0 z-50">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-        </div>
         {children}
       </main>
 
