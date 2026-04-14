5226574990b3cff344201140e0c597edd70444ca
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2026-04-14 17:06:17 +0700

    Update vite.config.ts

--- music/vite.config.ts
+++ music/vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-      allowedHosts: true, // Cho phép tất cả host (dùng khi dev qua tunnel)
-    },
     plugins: [react(), tailwindcss()],
     // Để Vite có thể thay thế biến môi trường khi build
     define: {
